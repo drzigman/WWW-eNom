3fee600d325779bdf18cd7cd57ae853b3a8d435d
Robert Stone <robertstone@hostgator.com>
2016-07-19 16:16:33 -0500

    Created POD for WWW::eNom::Role::Command::Contact

--- lib/WWW/eNom/Role/Command/Contact.pm
+++ lib/WWW/eNom/Role/Command/Contact.pm
@@ -24,7 +24,7 @@ Readonly my $ENOM_CONTACT_TYPE_MAPPING => {
 requires 'submit';
 
 # VERSION
-# ABSTRACT: Contact API Calls
+# ABSTRACT: Contact Related Operations
 
 sub get_contacts_by_domain_name {
     my $self = shift;
@@ -91,3 +91,61 @@ sub get_contacts_by_domain_name {
 1;
 
 __END__
+
+=head1 NAME
+
+WWW::eNom::Role::Command::Contact - Contact Related Operations
+
+=head1 SYNOPSIS
+
+    use WWW::eNom;
+    use WWW::eNom::Contact;
+
+    my $api = WWW::eNom->new( ... );
+
+    # Get Contacts for a Domain
+    my $contacts = $api->get_contacts_by_domain_name( 'drzigman.com' );
+
+    for my $contact_type (qw( registrant_contact admin_contact technical_contact billing_contact )) {
+        print "Email Address of $contact_type is: " . $contacts->{$contact_type}->email . "\n";
+    }
+
+=head1 REQUIRES
+
+=over 4
+
+=item submit
+
+=back
+
+=head1 DESCRIPTION
+
+Implements contact related operations with L<eNom|https://www.enom.com>'s API.
+
+=head1 METHODS
+
+=head2 get_contacts_by_domain_name
+
+    my $contacts = $api->get_contacts_by_domain_name( 'drzigman.com' );
+
+    for my $contact_type (qw( registrant_contact admin_contact technical_contact billing_contact )) {
+        print "Email Address of $contact_type is: " . $contacts->{$contact_type}->email . "\n";
+    }
+
+Given a FQDN, returns a HashRef of contacts for that domain.  The keys for the returned HashRef are:
+
+=over 4
+
+=item registrant_contact
+
+=item admin_contact
+
+=item technical_contact
+
+=item billing_contact
+
+=back
+
+Each of these keys point to a value which is an instance of L<WWW::eNom::Contact>.
+
+=cut
